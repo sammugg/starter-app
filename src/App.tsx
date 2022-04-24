bcdbf1cefa60fda5c7baba234e1f7ce778bf86f9
Sam Muggleworth <scmuggs@gmail.com>
2022-04-24 13:39:43 -0400

    Add router and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<>Nothing here yet</>} />
+          <Route path='/dashboard' element={<>Nothing here yet</>} />
+          <Route path='*' element={<>404 Not found</>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='Home'>
+      <header className='Home-header'>
+        <img src={logo} className='Home-logo' alt='logo' />
+        <p>
+          Edit
+          {' '}
+          <code>src/Home.tsx</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className='Home-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
